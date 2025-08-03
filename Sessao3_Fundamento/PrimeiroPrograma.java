ce93a0f2eece38ae84dff9cdf7b873973944caf6
Kemelly Klaricy <kemellyklaricydev@gmail.com>
2025-08-03 11:17:13 -0300

    Primeiro Exercicio do Curso
    Exercício simples e básico!

---
+++ Sessao3_Fundamento/PrimeiroPrograma.java
@@ -0,0 +1,6 @@
+public class PrimeiroPrograma {
+    public void main(String[] args) {
+        System.out.println("Hello, Word!");
+        System.out.println("Este é o meu exercicio do Curso de Java!");
+    }
+}
