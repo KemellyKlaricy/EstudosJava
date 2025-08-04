7e5695d1e8bb1fa640c327acf25567b42c45694f
Kemelly Klaricy <kemellyklaricydev@gmail.com>
2025-08-04 20:08:10 -0300

    código de teste para casting
    A diferença entre conversão implícita (quando o Java faz sozinho) e explícita (quando a gente força com ())

---
+++ Sessao3_Fundamento/ConversãoTiposPrimitivosNumericos.java
@@ -0,0 +1,25 @@
+public class ConversãoTiposPrimitivosNumericos {
+    public static void main(String[] args) {
+        
+        double a = 1; //Conversão Implícita
+        System.out.println(a); //Sem perca de informação
+
+        float b = (float) 1.0; //Conversão Explicita: Sem perca de informação
+        float ba = 1.12345678888F; //Conversão Explicita: Aqui ja perdeu informação
+        System.out.println(b);
+        System.out.println(ba);
+
+        int c = 130;
+        //byte d = c; - Aqui daria erro, pois o int é muito maior que o byte
+        byte d = (byte) c; //Conversão Explicita: Aqui ja perdeu informação e a conversão fica trucada
+        System.out.println(d);
+
+        double e = 1.9999999;
+        int f = (int) e; //Conversão Explicita: Aqui ja perdeu informação e a conversão fica trucada
+        System.out.println(f);
+
+        
+
+
+    }
+}
