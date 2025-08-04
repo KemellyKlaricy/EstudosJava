9384418a0f01ff5affaf419ac45b2dcf537908be
Kemelly Klaricy <kemellyklaricydev@gmail.com>
2025-08-04 18:38:45 -0300

    Pegar informações via Console
    Pegar informações do usuário através do Scanner

---
+++ Sessao3_Fundamento/Console.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Console {
+    public static void main(String[] args) {
+        
+        //Três formas de jogar dados no console
+        System.out.println("Bom");
+        System.out.println("dia!\n\n");
+
+        System.out.println("Bom");
+        System.out.println("dia!");
+
+        System.out.printf("Megasena: %d %d %d %d %d %d %n" , 1, 2, 3, 4, 5, 6);
+        System.out.printf("Salario %.1f", 1234.5678);
+
+        //Forma de pegar informações do usuario atraves do console
+        
+        Scanner entrada = new Scanner(System.in); //Abrir Scanner
+
+        System.out.print("\nDigite o seu nome: ");
+        String nome = entrada.nextLine(); //Guardar as informações de String no scanner
+
+        System.out.print("Digite o seu Sobrenome: ");
+        String sobrenome = entrada.nextLine();//Guardar as informações de String no scanner
+
+        System.out.println("Digite sua idade: ");
+        int idade = entrada.nextInt();//Guardar as informações de int no scanner
+
+        System.out.printf("%s %s tem %d anos.", nome, sobrenome, idade);
+
+        entrada.close(); //Fechar Scanner
+        
+        
+    }
+}
