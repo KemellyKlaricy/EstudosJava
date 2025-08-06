7596d8a98e4e1201e36acb14f42fb4183220f094
Kemelly Klaricy <kemellyklaricydev@gmail.com>
2025-08-05 23:17:29 -0300

    While Indeterminado

---
+++ Sessao4_Estruturas/CodigosAulas/WhileIndeterminado.java
@@ -0,0 +1,22 @@
+package Sessao4_Estruturas.CodigosAulas;
+
+import java.util.Scanner;
+
+public class WhileIndeterminado {
+    public static void main(String[] args) {
+        
+        Scanner entrada = new Scanner(System.in);
+
+        String valor = "";
+        
+        while(!valor.equalsIgnoreCase("sair")){
+            System.out.println("Digite a palavra: ");
+            valor = entrada.nextLine();
+        }
+
+        System.out.println("------------------------");
+        System.out.println("Saindo ...");
+
+        entrada.close();
+    }
+}
