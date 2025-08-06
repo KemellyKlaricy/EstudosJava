da3fff991be108e1dbae043c5706e94dcdc3fc07
Kemelly Klaricy <kemellyklaricydev@gmail.com>
2025-08-05 23:53:37 -0300

    Desafio For sem usar valor numérico

---
+++ Sessao4_EstruturasDeControle/Exercicios/DesafioFor.java
@@ -0,0 +1,11 @@
+package Sessao4_EstruturasDeControle.Exercicios;
+
+public class DesafioFor {
+    public static void main(String[] args) {
+
+        //controlar o laço FOR sem usar valor numérico
+        for(String v = "#"; !v.equals("#####"); v += "#"){
+            System.out.println(v);
+        }
+    }
+}
