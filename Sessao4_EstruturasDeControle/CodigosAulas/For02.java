f69027386db8c9aa41e713c47030a5db30e7d157
Kemelly Klaricy <kemellyklaricydev@gmail.com>
2025-08-05 23:37:40 -0300

    Estudando FOR p.02

---
+++ Sessao4_EstruturasDeControle/CodigosAulas/For02.java
@@ -0,0 +1,10 @@
+package Sessao4_EstruturasDeControle.CodigosAulas;
+
+public class For02 {
+    public static void main(String[] args) {
+        
+        for(int contador = 10; contador >= 10; contador -= 2){
+            System.out.printf("Contador %d\n", contador);
+        }
+    }
+}
