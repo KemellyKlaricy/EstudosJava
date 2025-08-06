0f05ab629c9f0e0bbef8757d991cc16e7d140db7
Kemelly Klaricy <kemellyklaricydev@gmail.com>
2025-08-05 23:12:55 -0300

    Estudos da estrutura FOR #01

---
+++ Sessao4_Estruturas/CodigosAulas/For01.java
@@ -0,0 +1,12 @@
+package Sessao4_Estruturas.CodigosAulas;
+
+public class For01 {
+    public static void main(String[] args) {
+
+        for(int contador = 0; contador <= 10; contador++){
+            System.out.println("Bom dia!");
+        }
+
+
+    }
+}
