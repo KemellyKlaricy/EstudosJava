ff58be47f43b8d79b0b1053bc7f8d29495b722ac
Kemelly Klaricy <kemellyklaricydev@gmail.com>
2025-08-05 22:28:26 -0300

    Estrutura IF

---
+++ Sessao4_Estruturas/CodigosAulas/If.java
@@ -0,0 +1,26 @@
+package Sessao4_Estruturas.CodigosAulas;
+
+import java.util.Scanner;
+
+public class If {
+    public static void main(String[] args) {
+        Scanner entrada =  new Scanner(System.in);
+
+        System.out.println("Informe a média: ");
+        double media = entrada.nextDouble();
+
+        if(media >= 7.0){
+            System.out.println("Aprovado!");
+        }
+        if(media < 7.0 && media >= 4){
+            System.out.println("Recuperação!");
+        }
+        if(media < 4){
+            System.out.println("Reprovado!");
+        }
+
+
+
+        entrada.close();
+    }
+}
