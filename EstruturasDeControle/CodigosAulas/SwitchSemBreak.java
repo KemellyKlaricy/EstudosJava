361f3204b379f266172dcdc3eb7de34d4a9aa9f6
Kemelly Klaricy <kemellyklaricydev@gmail.com>
2025-08-07 09:30:16 -0300

    Ajustes

--- EstruturasDeControle/CodigosAulas/SwitchSemBreak.java
+++ EstruturasDeControle/CodigosAulas/SwitchSemBreak.java
@@ -20,8 +20,23 @@ public class SwitchSemBreak {
                 System.out.println("Seu o  Heian Nidan");
             case "amarela":
                 System.out.println("Sei o Heian Shodan");
-           // default:
-          //    System.out.println("Não sei nada");
+                break;
+           default:
+                System.out.println("Não sei nada");
+        }
+
+        System.out.println("Fim!");
+
+        int idade = 3;
+        switch(idade){
+            case 3:
+                System.out.println("Sabe programar");
+            case 2:
+                System.out.println("Sabe falar");
+            case 1:
+                System.out.println("Sabe andar");
+            case 0:
+                System.out.println("Sabe respirar");
         }
     }
 }
