e1ecf13f32b0eddf8c855ed0a746ea77e71bc927
Kemelly Klaricy <kemellyklaricydev@gmail.com>
2025-08-08 23:08:01 -0300

    Refatorando as classes

--- ClassesMetodos/Produto.java
+++ ClassesMetodos/Produto.java
@@ -5,5 +5,15 @@ public class Produto {
     String nome;
     double preço;
     double desconto;
+
+    double precoComDesconto(){
+        return preço * (1 - desconto);
+    }
+
+    double precoComDesconto(double descontoGerente){
+        return preço * (1 - (desconto + descontoGerente));
+    }
+
+
     
 }
--- ClassesMetodos/ProdutoTeste.java
+++ ClassesMetodos/ProdutoTeste.java
@@ -18,11 +18,12 @@ public class ProdutoTeste {
         System.out.println(p1.nome);
         System.out.println(p2.nome);
        
-        double precoFinal1 = p1.preço * (1 - p1.desconto);
-        double precoFinal2 = p2.preço * (1 - p2.desconto);
+        double precoFinal1 = p1.precoComDesconto();
+        double precoFinal2 = p2.precoComDesconto(0.1);
         double mediaCarrinho = (precoFinal1 + precoFinal2) / 2;
 
         System.out.printf("Média do carrinho = R$%.2f", mediaCarrinho);
       
+        System.out.println();
     }
 }
