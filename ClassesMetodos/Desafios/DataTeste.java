489742aab1a0dbf49e32528122368bf2118c28a5
Kemelly Klaricy <kemellyklaricydev@gmail.com>
2025-08-08 22:34:13 -0300

    Ajustes

--- ClassesMetodos/Desafios/DataTeste.java
+++ ClassesMetodos/Desafios/DataTeste.java
@@ -14,6 +14,8 @@ public class DataTeste {
         System.out.println("Digite o ano: ");
         data1.ano = entrada.nextInt();
 
+        System.out.println("A data digitada é: " + data1.dia + " / " + data1.mês + " / " + data1.ano);
+
         entrada.close();
 
     }
